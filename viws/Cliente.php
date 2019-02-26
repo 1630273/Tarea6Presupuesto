0ea8e261f8f7985ce883663e72c742d93ff66326
1630273 <1630273@upv.edu.mx>
2019-02-26 01:56:01 -0600

    first commit

---
+++ viws/Cliente.php
@@ -0,0 +1,7 @@
+<?php
+    include"Plantilla.php";
+    require_once"core/Config.php";
+   // require_once"controller/vistasControlador.php";
+
+   // $plantilla = new vistasControlador();
+   // $plantilla->obtener_plantilla_controlador();
